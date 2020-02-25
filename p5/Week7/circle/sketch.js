24ab0abce9e206d65d7b0629d04996c2c37d4f94
Jeremiah Risinger <risinger.jeremiah@gmail.com>
2020-02-25 09:14:42 -0600

    week 6 and 7

---
+++ p5/Week7/circle/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  // put setup code here
+  createCanvas(500, 500);
+}
+
+function draw() {
+  // put drawing code here
+  //nostroke();
+  fill(20);
+  ellipse(mouseX, mouseY, 2, 2) ;
+}

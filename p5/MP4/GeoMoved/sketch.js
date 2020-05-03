22bb8e69e8fef1a9a7c75238d6c8098814ea579d
Jeremiah Risinger <risinger.jeremiah@gmail.com>
2020-05-03 16:10:54 -0500

    ugh

--- p5/MP4/GeoMoved/sketch.js
+++ p5/MP4/GeoMoved/sketch.js
@@ -27,27 +27,27 @@ function positionPing(position) {
 	text("number of updates " + num, 10, 140);
 
 	distance = calcGeoDistance(locationData.latitude, locationData.longitude, position.latitude, position.longitude, 'mi');
-	text("you have moved " + distance, 10, 190);
-	YouMoved(distance);
+	text("you have moved " + distance * scale, 10, 190);
+	YouMoved(distance * scale);
 
 
 }
 
 function YouMoved(distance) {
-	if (distance > 20 / scale)
+	if (distance > 20)
 		text("okay yeah stop before you kill your self....", 10, 220);
-	else if (distance > 15 / scale)
+	else if (distance > 15)
 		text("Woow.. thats alot...", 10, 220);
-	else if (distance > 10 / scale)
+	else if (distance > 10)
 		text("nice!", 10, 220);
-	else if (distance > 5 / scale)
+	else if (distance > 5)
 		text("heeeyyy... nice your getting somewhere", 10, 220);
-	else if (distance > 3 / scale)
+	else if (distance > 3)
 		text("okay, okay.. but dont stop now... jeez", 10, 220);
-	else if (distance > 1 / scale)
+	else if (distance > 1)
 		text("ehh... you should probably keep going...", 10, 220);
-	else if (distance > 0.5 / scale)
+	else if (distance > 0.5)
 		text("Wow... congrats you moved...", 10, 220);
-	else if (distance < 0.5 / scale)
+	else if (distance < 0.5)
 		text("k... you are at your starting point...how about you try moving", 10, 220);
 }
